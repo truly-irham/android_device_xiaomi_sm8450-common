2f74414a3f005b5c51f665192d023e543896ecfe
Arian <arian.kulmer@web.de>
2024-04-10 00:20:48 +0200

    sm8450-common: udfps: Only disable fod_status until suspend
    Change-Id: If4d9084cbcc8670c7636fddd143807cb6b067f22

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -32,6 +32,7 @@
 
 #define FOD_STATUS_OFF 0
 #define FOD_STATUS_ON 1
+#define FOD_STATUS_OFF_UNTIL_SUSPEND 3
 
 #define TOUCH_DEV_PATH "/dev/xiaomi-touch"
 #define TOUCH_MAGIC 'T'
@@ -225,7 +226,7 @@ class XiaomiSm8450UdfpsHander : public UdfpsHandler {
             ioctl(disp_fd_.get(), MI_DISP_IOCTL_SET_LOCAL_HBM, &req);
 
             if (!enrolling) {
-                setFodStatus(FOD_STATUS_OFF);
+                setFodStatus(FOD_STATUS_OFF_UNTIL_SUSPEND);
             }
         }
 
@@ -248,7 +249,7 @@ class XiaomiSm8450UdfpsHander : public UdfpsHandler {
         LOG(DEBUG) << __func__;
         enrolling = false;
 
-        setFodStatus(FOD_STATUS_OFF);
+        setFodStatus(FOD_STATUS_OFF_UNTIL_SUSPEND);
     }
 
     void preEnroll() {
@@ -265,7 +266,7 @@ class XiaomiSm8450UdfpsHander : public UdfpsHandler {
         LOG(DEBUG) << __func__;
         enrolling = false;
 
-        setFodStatus(FOD_STATUS_OFF);
+        setFodStatus(FOD_STATUS_OFF_UNTIL_SUSPEND);
     }
 
   private:
